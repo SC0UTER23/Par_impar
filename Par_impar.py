5c4264956129a6861bfd92b06fd59b09c6b193ec
SC0UTER23 <juanduran2@colegioguanenta.edu.co>
2025-02-05 06:58:10 -0500

    sigma

---
+++ Par_impar.py
@@ -0,0 +1,8 @@
+# Solicitar al usuario un número
+numero = int(input("🌟 Introduce un número: "))
+
+# Verificar si el número es par o impar
+if numero % 2 == 0:
+    print(f"🎉 ¡El número {numero} es par! 🎉")
+else:
+    print(f"😎 ¡El número {numero} es impar! 😎")
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Par_impar
+Programa para saber si un numero es par o impar
+
+## input
+
+escribe el numero que quieres averiguar si es par o impar
+
+## Oupot
+saber si el numero escrito es par o impar
+## Diseño
+![diagramadeflujo](diagrama23.png)
